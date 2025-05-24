be6b8d3b021d13fbed98d633c680c58ce32a9f7e
Anastasiia <gorgona1597531597@gmail.com>
2025-05-24 18:52:44 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     let filtered = [];
-    for (let i = 0; i <= numbers.length; i += 1) {
+    for (let i = 0; i < numbers.length; i += 1) {
         if (numbers[i] > value) {
             filtered.push(numbers[i]);
         }
